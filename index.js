dcc6cf433be487a24ea345efd9765db3812b5d7c
Fernando Judice <fernandocesar95@gmail.com>
2024-12-23 23:39:51 -0300

    working game logic. check for wins, prevent same spot play

--- index.js
+++ index.js
@@ -8,34 +8,65 @@ const board = (function() {
         return _board[size[0]*x+y];
     }
     const setSpot = (x,y,player) => {_board[size[0]*x+y] = player;};
-    return {getSpot, setSpot}
+    const getRow = (x) => [_board[x*size[0]+0], _board[x*size[0]+1], _board[x*size[0]+2]]
+    const getCol = (x) => [_board[0*size[0]+x], _board[1*size[0]+x], _board[2*size[0]+x]]
+    const getDiagCres = () => [_board[2*size[0]+0], _board[4], _board[2]]
+    const getDiagDesc = () => [_board[0], _board[4], _board[8]]
+    const printBoard = () => console.log(_board)
+    return {getSpot, setSpot, getRow, getCol, getDiagCres, getDiagDesc, printBoard}
 })();
 
 
 function createPlayer(player) {
-    const player = player;
-    const get = () => player;
+    const value = player;
+    const get = () => value;
     return {get}
 }
 
 
 const turnManager = (function() {
-    const turn = 0;
+    let turn = 0;
     const getTurn = () => turn;
     const passTurn = () => turn++;
+    return {getTurn, passTurn}
 })();
 
 
-const game = (function(board, player1, player2, turnManager) {
+function createGame (board, turnManager) {
     const _board = board;
-    const _player1 = player1;
-    const _player2 = player2;
+    const _player1 = createPlayer(1);
+    const _player2 = createPlayer(2);
     const _turnManager = turnManager;
-    const _activePlayer = () => _turnManager.getTurn % 2 == 0 ? player1.get() : player2.get();
+    const _activePlayer = () => _turnManager.getTurn() % 2 == 0 ? _player1.get() : _player2.get();
 
-    const play = (x,y) => {
-        board.setSpot(x,y,_activePlayer());
-        _turnManager.passTurn();
+    const _checkLine = (line) => {
+        let result = true;
+        for (let spot of line) {
+            if (spot == undefined || spot != line[0]) {
+                return false
+            }
+        }
+        return result
     }
 
-})();
+    const play = (x,y) => {
+        if (board.getSpot(x,y) == _player1.get() || board.getSpot(x,y) == _player2.get()) {
+            return
+        }
+        board.setSpot(x,y,_activePlayer());
+        if (_checkLine(_board.getRow(x))||_checkLine(_board.getCol(y))||_checkLine(_board.getDiagCres()||_checkLine(_board.getDiagDesc()))){
+            console.log(_activePlayer() + " Won!")
+        };
+        board.printBoard();
+        _turnManager.passTurn();
+    }
+    return {play}
+};
+
+const game = createGame(board,turnManager)
+game.play(0,0);
+game.play(0,0);
+game.play(1,0);
+game.play(0,1);
+game.play(1,1);
+game.play(0,2);
